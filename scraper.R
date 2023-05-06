7714e403e3311f2cc34d81cddee53602ee5e79fa
Linus Stuhlmann <94235012+slinusc@users.noreply.github.com>
2023-05-06 18:31:35 +0200

    Add files via upload

---
+++ scraper.R
@@ -0,0 +1,15 @@
+library(rvest)
+
+url <- "https://fis.fda.gov/extensions/FPD-QDE-FAERS/FPD-QDE-FAERS.html"
+
+links <- read_html(url) |>
+  html_nodes(xpath = '//td[@class="fpd-td"]/a') |>
+  html_attr("href")
+
+print(links)
+
+for (i in 1:5){
+  local_file <- gsub("https://fis.fda.gov/content/Exports/", "data/zip_files/", links[i])
+  curl::curl_download(links[i], destfile = local_file)
+  unzip(local_file, exdir = "data/")
+}
